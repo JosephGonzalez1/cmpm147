8b5789e25bcfb88586fa5c01cbbb05c5db2f9eed
JosephGonzalez1 <152119091+JosephGonzalez1@users.noreply.github.com>
2025-04-08 20:14:18 -0700

    Change to code to allow for it to properly display

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,56 +23,52 @@ class MyProjectClass {
 }
 
 function main() {
-  const fillers = {
-    adventurer: ["Person", "Guy", "AI", "Nomad", "Hollowed One", "Fella", "Villager"],
-    pre: ["Hollow", "Death", "Green", "Ice", "Rock", "Fleet"],
-    post: ["town", "village", "commune"],
-    people: ["people", "okay", "crazy", "funny", "dead", "uncanny", "hateful"],
-    item: ["twinaxe", "blade of flight", "burnt book", "cloaked cloak", "bubble shield", "fish"],
-    num: ["like maybe two", "a couple", "exactly 21", "some but not more than four"],
-    looty: ["trashy", "marketable", "serviceable", "kinda rare but not too rare", "cool but in a bad way"],
-    loots: ["good points", "Best Dad Mugs", "copper coins", "cow leathers but the bad kind", "dudes"],
-    baddies: ["dragons", "blobs of slime", "just some regular people", "a singular rabbit", "like a lot of bears like too many bears"],
-    message: ["whimper", "wechat", "whistle", "request", "twitter post"],
-    
-  };
+  function main() {
+    const fillers = {
+      adventurer: ["Person", "Guy", "AI", "Nomad", "Hollowed One", "Fella", "Villager"],
+      pre: ["Hollow", "Death", "Green", "Ice", "Rock", "Fleet"],
+      post: ["town", "village", "commune"],
+      people: ["people", "okay", "crazy", "funny", "dead", "uncanny", "hateful"],
+      item: ["twinaxe", "blade of flight", "burnt book", "cloaked cloak", "bubble shield", "fish"],
+      num: ["like maybe two", "a couple", "exactly 21", "some but not more than four"],
+      looty: ["trashy", "marketable", "serviceable", "kinda rare but not too rare", "cool but in a bad way"],
+      loots: ["good points", "Best Dad Mugs", "copper coins", "cow leathers but the bad kind", "dudes"],
+      baddies: ["dragons", "blobs of slime", "just some regular people", "a singular rabbit", "like a lot of bears like too many bears"],
+      message: ["whimper", "wechat", "whistle", "request", "twitter post"],
+    };
   
-  const template = `Please $adventurer, listen to my $message!
+    const template = `Please $adventurer, listen to my $message!
   
   I come from $pre$post where the $people folk are in need your help. Their town has been taken over by $baddies. We need you to go there, taking this totally free $item, and help them in anyway you can.
   
-  I'll reward you with $num $looty $loots. This is a good quest I swear!
-  `;
+  I'll reward you with $num $looty $loots. This is a good quest I swear!`;
   
+    const box = document.getElementById("box");
+    const clicker = document.getElementById("clicker");
   
-  // STUDENTS: You don't need to edit code below this line.
+    const slotPattern = /\$(\w+)/;
   
-  const slotPattern = /\$(\w+)/;
-  
-  function replacer(match, name) {
-    let options = fillers[name];
-    if (options) {
-      return options[Math.floor(Math.random() * options.length)];
-    } else {
-      return `<UNKNOWN:${name}>`;
-    }
-  }
-  
-  function generate() {
-    let story = template;
-    while (story.match(slotPattern)) {
-      story = story.replace(slotPattern, replacer);
+    function replacer(match, name) {
+      let options = fillers[name];
+      if (options) {
+        return options[Math.floor(Math.random() * options.length)];
+      } else {
+        return `<UNKNOWN:${name}>`;
+      }
     }
   
-    /* global box */
-    box.innerText = story;
+    function generate() {
+      let story = template;
+      while (story.match(slotPattern)) {
+        story = story.replace(slotPattern, replacer);
+      }
+      box.innerText = story;
+    }
+  
+    clicker.onclick = generate;
+    generate();
   }
-  
-  /* global clicker */
-  clicker.onclick = generate;
-  
-  generate();
 }
 
 // let's get this party started - uncomment me
-main();
+window.onload = main;
